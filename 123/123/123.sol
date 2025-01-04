def2e772374f648aa2d1b6e71890b56a1b052ea8
Kanthub <564080210@qq.com>
2025-01-04 15:54:18 +0800

    123

---
+++ 123/123/123.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.28;
+
+contract abc {
+
+}
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
